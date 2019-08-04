cf29791ccca0730d24a24382f62ba684bd2d38b1
gary <eguoli@gmail.com>
2019-08-04 20:51:27 +0800

    fixed structure bug

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
-import { ReactComponent as ReactLogo } from "./logo.svg";
-import { BrowserRouter } from "react-router-dom";
+// import { ReactComponent as ReactLogo } from "./logo.svg";
+
 import Routes from "./routes";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-
 import createSagaMiddleware from "redux-saga";
 import rootReducer from "./redux/reducers";
 import rootSaga from "./sagas";
@@ -15,9 +14,7 @@ sagaMiddleware.run(rootSaga);
 
 const AppRouter = () => (
   <Provider store={store}>
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <Routes />
   </Provider>
 );
 export default AppRouter;
--- reactjs/src/routes/index.js
+++ reactjs/src/routes/index.js
@@ -2,17 +2,24 @@ import React from "react";
 // import styled from "styled-components";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-
 import Home from "../pages/Home";
 import NoMatch from "../pages/error/404";
 // import NoMatch from "../pages/404";
 
+const App = () => (
+  <div className='App'>
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route path='/error/404' component={NoMatch} />
+      {/* <Route path="/:name" component={Package} /> */}
+    </Switch>
+  </div>
+);
+
 const AppRouter = () => (
-  <Switch>
-    <Route exact path='/' component={Home} />
-    <Route path='/error/404' component={NoMatch} />
-    {/* <Route path="/:name" component={Package} /> */}
-  </Switch>
+  <Router>
+    <App />
+  </Router>
 );
 
 export default AppRouter;
